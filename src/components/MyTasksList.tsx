13eeb1f477d5e443e73c65e054bc63f57d285baf
pedrofilipe <pedro.paraguacu@gmail.com>
2021-05-20 23:23:24 -0300

    correcoes

--- src/components/MyTasksList.tsx
+++ src/components/MyTasksList.tsx
@@ -27,15 +27,20 @@ export function MyTasksList({ tasks, onLongPress, onPress }: MyTasksListProps) {
       renderItem={({ item, index }) => {
         return (
           <TouchableOpacity
+            style={[item.done ? styles.taskButtonDone : styles.taskButton]}
             testID={`button-${index}`}
             activeOpacity={0.7}
+            onPress={()=> onPress(item.id)}
+            onLongPress={() => onLongPress(item.id)}
             //TODO - use onPress, onLongPress and style props
           >
             <View 
               testID={`marker-${index}`}
+              style={[item.done ? styles.taskMarkerDone : styles.taskMarker]}
               //TODO - use style prop 
             />
             <Text 
+            style={[item.done ? styles.taskTextDone : styles.taskText]}
               //TODO - use style prop
             >
               {item.title}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,17 +11,42 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    if(newTaskTitle!=''){
+      const data = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false
+      };
+
+      setTasks(oldState=> [...oldState, data]);
+    }
     //TODO - add new task if it's not empty
   }
 
   function handleMarkTaskAsDone(id: number) {
+
+    if(tasks.findIndex(task=> task.id == id)>-1){
+
+      let itens = [...tasks];
+      let item = itens.find(task=>task.id == id);
+
+      itens[itens.findIndex(task=>task.id == id)] = {
+        id: id,
+        title: String(item?.title),
+        done: !item?.done
+      };
+
+      setTasks(itens);
+    }
+
     //TODO - mark task as done if exists
   }
 
   function handleRemoveTask(id: number) {
+    setTasks(oldTasks => oldTasks.filter(task=> task.id !== id));
     //TODO - remove task from state
   }
 
